950cf18592fe5f99a10d229f685f3ff3e91c7376
Shaik Suhana <suhanashaik044@gmail.com>
2026-06-07 19:21:29 +0530

    d

--- src/Pages/DemographicPage.tsx
+++ src/Pages/DemographicPage.tsx
@@ -209,7 +209,7 @@ function DemographicPage() {
             <label className="
               text-lg
               font-bold
-              text-slate-700
+              text-slate-700 mt-3
             ">
               Gender
             </label>
